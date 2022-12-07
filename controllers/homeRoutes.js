8592abe18dca4cb9da4f32b6c5c92133c58d2ab0
EdenABE <eden.a.biltibo@vumc.org>
2022-12-07 14:07:27 -0500

    file structure

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,69 @@
+const router = require("express").Router();
+const { Post, Comment, User } = require("../models");
+
+router.get("/", async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const postData = await Post.findAll({
+      include: [
+        User
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render("homepage", {
+      posts
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/post/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        User,
+        {
+          model: Comment,
+          include: ["User"],
+        },
+      ],
+    });
+
+    if (postData) {
+    const post = postData.get({ plain: true });
+
+    res.render("single-post", {
+      post
+    });
+  } else (err) {
+      res.status(404).end(err);
+  }
+} catch (err) {
+  res.status(500).json(err);
+}
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('signup');
+});
+
+module.exports = router;
